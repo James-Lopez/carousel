012b2972ff7a6531ee8e3c8fcd96b3c4e577c23a
James Lopez <jl71491@gmail.com>
2017-10-24 15:53:37 -0700

    Completed User can view images in carousel.

--- public/main.js
+++ public/main.js
@@ -1,10 +1,23 @@
-const images = data =>
-  data.map(item => ({
-    id: item._id,
-    src: item.image
-  }))
+async function slides() {
+  const response = await fetch('/carousel')
+  return await response.json()
+}
 
-fetch('/carousel')
-  .then(res => res.json())
-  .then(data => images(data))
-  .then(items => console.log(items))
+const carousel = {
+  slides: null,
+  current: 0
+}
+
+slides().then(documents => (carousel.slides = documents))
+
+setInterval(function() {
+  document
+    .querySelector('#carousel')
+    .setAttribute('src', `${carousel.slides[carousel.current].image}`)
+
+  if (carousel.current < carousel.slides.length - 1) {
+    carousel.current++
+  } else {
+    carousel.current = 1
+  }
+}, 3500)
