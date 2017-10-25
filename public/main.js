17b98e8c0bb680d034db2b51b6ce1b22ef620477
James Lopez <jl71491@gmail.com>
2017-10-25 14:16:45 -0700

    Completed indicator function and styling.

--- public/main.js
+++ public/main.js
@@ -8,33 +8,61 @@ const carousel = {
   current: 0
 }
 
+slides().then(documents => (carousel.slides = documents))
+
 function switchSlide() {
   document
     .querySelector('#carousel')
     .setAttribute('src', `${carousel.slides[carousel.current].image}`)
 }
 
-slides().then(documents => (carousel.slides = documents))
-
 setInterval(() => {
-  if (0 < carousel.current < carousel.slides.length - 1) {
+  if (carousel.current < carousel.slides.length - 1) {
     carousel.current++
   } else {
     carousel.current = 0
   }
   switchSlide()
+  progress()
 }, 3000)
 
 document.querySelector('.left').addEventListener('click', () => {
-  if (carousel.current === 0) return
   carousel.current--
-  console.log('left, now on #' + carousel.current)
+  if (carousel.current < 0) {
+    carousel.current = carousel.slides.length - 1
+  }
+  console.log('decrement, now on #' + carousel.current)
   switchSlide()
+  progress()
 })
 
 document.querySelector('.right').addEventListener('click', () => {
-  if (carousel.current === carousel.slides.length - 1) return
   carousel.current++
-  console.log('right, now on #' + carousel.current)
+  if (carousel.current > carousel.slides.length - 1) {
+    carousel.current = 0
+  }
+  console.log('increment, now on #' + carousel.current)
   switchSlide()
+  progress()
 })
+
+const indicator = document.querySelector('#indicator-container')
+
+const progress = () => {
+  const $indicators = carousel.slides.map((slide, index) => {
+    const $indicator = document.createElement('div')
+
+    if (index === carousel.current) {
+      $indicator.className = 'indicator active'
+    } else {
+      $indicator.className = 'indicator inactive'
+    }
+
+    return $indicator
+  })
+  indicator.innerHTML = ''
+  $indicators.reduce((parent, child) => {
+    parent.appendChild(child)
+    return parent
+  }, indicator)
+}
