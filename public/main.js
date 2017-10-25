0cd2ceb62359791e81ae9a6a2172c5854a4be4ea
James Lopez <jl71491@gmail.com>
2017-10-25 11:05:07 -0700

    Added initial styling for left/right icons along with initial function to manipulate slide position.

--- public/main.js
+++ public/main.js
@@ -8,16 +8,33 @@ const carousel = {
   current: 0
 }
 
-slides().then(documents => (carousel.slides = documents))
-
-setInterval(function() {
+function switchSlide() {
   document
     .querySelector('#carousel')
     .setAttribute('src', `${carousel.slides[carousel.current].image}`)
+}
 
-  if (carousel.current < carousel.slides.length - 1) {
+slides().then(documents => (carousel.slides = documents))
+
+setInterval(() => {
+  switchSlide()
+  if (0 < carousel.current < carousel.slides.length - 1) {
     carousel.current++
-  } else {
+  } else if (carousel.current > carousel.slides.length - 1) {
     carousel.current = 1
+  } else if (carousel.current < 0) {
+    carousel.current = carousel.slides.length - 1
   }
-}, 3500)
+}, 3000)
+
+document.querySelector('.left').addEventListener('click', () => {
+  carousel.current--
+  console.log('left, now on #' + carousel.current)
+  switchSlide()
+})
+
+document.querySelector('.right').addEventListener('click', () => {
+  carousel.current++
+  console.log('right, now on #' + carousel.current)
+  switchSlide()
+})
